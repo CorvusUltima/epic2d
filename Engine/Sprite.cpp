23b62cfa971ce2bf366e6d94f3a9cb2c5319648d
Nemanja Pavlovic <pavlovicn88@gmail.com>
2021-05-13 12:51:35 +0200

    Created definitions for Sprite functions

--- Engine/Sprite.cpp
+++ Engine/Sprite.cpp
@@ -9,6 +9,67 @@ Sprite::Sprite(int width, int height)
 }
 
 Sprite::Sprite(const Sprite& rhs)
+	:
+	Sprite(rhs.width,rhs.height)
+{
+
+	const int nPixels = width * height;
+	for (int i = 0; i < nPixels; i++)
+	{
+		pPixels[i] = rhs.pPixels[i];
+	}
+
+}
+
+Sprite::~Sprite()
+{
+	delete[] pPixels;
+	pPixels = nullptr;
+}
+
+Sprite& Sprite::operator=(const Sprite& rhs)
+
 
 {
+	if (pPixels == rhs.pPixels)
+	{
+		return *this;
+	}
+	else
+
+		width = rhs.width;
+	    height = rhs.height;
+	    delete[] pPixels;
+	    pPixels = new Color[width * height];
+
+		const int nPixels = width * height;
+		for (int i = 0; i < nPixels; i++)
+		{
+			pPixels[i] = rhs.pPixels[i];
+		}
+
+	    return *this;
+	
+}
+
+void Sprite::PutPixel(int x, int y, Color c)
+{
+
+	pPixels[y * width + x] = c;
+
+}
+
+Color Sprite::GetPixel(int x, int y) const
+{
+	return pPixels[y * width + x];
+}
+
+int Sprite::GetWidth() const
+{
+	return width;
+}
+
+int Sprite::GetHeight() const
+{
+	return height;
 }
